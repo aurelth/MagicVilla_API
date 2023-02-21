71cbe4f856a36be433685c496a4c4975d5c779cd
Aurel <aurelth@icloud.com>
2023-02-21 20:02:35 -0300

    Controller and Villa Entity Creation

---
+++ MagicVilla_Api/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Api.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa{ Id=1, Name="Blumenau" },
+                new Villa{ Id=2, Name="Cotonou" }
+            };
+        }
+    }
+}
